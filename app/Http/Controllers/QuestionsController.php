8ef6bce67cbb5545778d564c6a0a232a5fdc5099
vagrant <vagrant@localhost.localdomain>
2019-02-26 23:40:59 +0900

    add author info and question creation date in question item

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -14,9 +14,11 @@ class QuestionsController extends Controller
      */
     public function index()
     {
-        $questions = Question::latest()->paginate(2);
+        // \DB::enableQueryLog();
+        $questions = Question::with('user')->latest()->paginate(2);
 
-        return view('questions.index' , compact('questions'));
+         return view('questions.index' , compact('questions'));
+         // dd(\DB::getQueryLog());
     }
 
     /**
